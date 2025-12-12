ea25409d8c68a5eaf934fbc35cd218c5a6ff33f9
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-12 18:03:07 +0700

    Initial Commit

---
+++ RatingApps/RatingAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingAppsApp.swift
+//  RatingApps
+//
+//  Created by Iqbal Alhadad on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RatingAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
